2500cbaa0f4b99f1679c571db97ef547487edad8
Libericatagua <marek.halas.147@gmail.com>
2021-03-16 17:19:42 +0100

    x

---
+++ Labs/05-counter/counter/counter.srcs/sim_1/new/tb_cmt_up_down.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 15.03.2021 22:46:04
+-- Design Name: 
+-- Module Name: tb_cmt_up_down - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_cmt_up_down is
+--  Port ( );
+end tb_cmt_up_down;
+
+architecture Behavioral of tb_cmt_up_down is
+
+begin
+
+
+end Behavioral;
